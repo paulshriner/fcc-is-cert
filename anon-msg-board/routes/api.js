9d0303dfda9f40130db177811fde28b5bf7f61bd
paulshriner <shrinerpaul@gmail.com>
2025-08-25 21:10:19 -0400

    anon-msg-board: reply to threads, add replies to thread get route

--- anon-msg-board/routes/api.js
+++ anon-msg-board/routes/api.js
@@ -66,7 +66,7 @@ const Thread = mongoose.model('Thread', threadSchema);
 const Reply = mongoose.model('Reply', replySchema);
 
 module.exports = app => {
-  app.route('/api/threads/:board')
+   app.route('/api/threads/:board')
       // route for creating a new thread   
       .post(async (req, res) => {
         if ((req.body.board || req.params.board) && req.body.text && req.body.delete_password) {
@@ -90,23 +90,45 @@ module.exports = app => {
         }
       })
       // route for retrieving thread information
-      .get((req, res) => {
+      .get(async (req, res) => {
         if (req.params.board) {
-          Thread.find({"board": req.params.board})
-                .then(u => {
+          await Thread.find({"board": req.params.board})
+                .then(async u => {
                   let threads = [];
 
+                  // extract needed information from db records
                   for (const thread in u) {
+                    // if replies exist, extract and sort them
+                    let replies = [];
+                    if (u[thread].replies) {
+                      await Reply.find({"_id": {"$in": u[thread].replies}})
+                           .then(v => {
+                            for (const reply in v) {
+                              replies.push({
+                                "_id": v[reply]._id,
+                                "text": v[reply].text,
+                                "created_on": v[reply].created_on
+                              });
+                             }
+                           })
+                           .catch(err => {
+                             console.log(err);
+                           });
+
+                      // Thanks https://www.geeksforgeeks.org/javascript/sort-an-object-array-by-date-in-javascript/ for sorting by date
+                      replies = replies.sort((a, b) => new Date(b.bumped_on) - new Date(a.bumped_on)).slice(0, 3);
+                    }
+                    
                     threads.push({
                       "_id": u[thread]._id,
                       "text": u[thread].text,
                       "created_on": u[thread].created_on,
                       "bumped_on": u[thread].bumped_on,
-                      "replies": [] // will need changed to support replies
+                      "replies": replies,
+                      "replycount": u[thread].replies.length
                     });
                   }
 
-                  // Thanks https://www.geeksforgeeks.org/javascript/sort-an-object-array-by-date-in-javascript/ for sorting by date
                   threads = threads.sort((a, b) => new Date(b.bumped_on) - new Date(a.bumped_on)).slice(0, 10);
 
                   res.json(threads);
@@ -121,6 +143,8 @@ module.exports = app => {
       // route for deleting a thread
       .delete((req, res) => {
         if (req.body.thread_id && req.body.delete_password) {
+          // find the thread, if it exists check password
+          // if password valid delete the thread
           Thread.findOne({"_id": req.body.thread_id})
                 .then(async u => {
                   if (u !== null) {
@@ -149,7 +173,8 @@ module.exports = app => {
       // route for reporting a thread
       .put((req, res) => {
         if (req.body.thread_id) {
-          Thread.findOneAndUpdate({"_id": req.body.thread_id}, {reported: true})
+          // find the thread, update its reported value
+          Thread.findOneAndUpdate({"_id": req.body.thread_id}, {"reported": true})
                 .then(u => {
                   if (u !== null) {
                     res.send("reported");
@@ -165,5 +190,33 @@ module.exports = app => {
         }
       });
     
-  app.route('/api/replies/:board');
+   app.route('/api/replies/:board')
+      // route for replying to a thread
+      .post(async (req, res) => {
+        if (req.body.text && req.body.delete_password && req.body.thread_id) {
+          // create a new reply
+          Reply.insertOne({
+            "text": req.body.text,
+            "delete_password": await bcrypt.hash(req.body.delete_password, saltRounds)
+          })
+          .then(u => {
+            // add the reply to the thread by appending its id
+            Thread.findOneAndUpdate({"_id": req.body.thread_id}, {
+              "bumped_on": Date.now(),
+              // Thanks https://stackoverflow.com/a/52347322 for push syntax
+              "$push": {"replies": u._id}
+            })
+            .catch(err => {
+              console.log(err);
+            })
+          })
+          .catch(err => {
+            console.log(err);
+          })
+
+          res.redirect('/b/' + req.body.board + '/' + req.body.thread_id);
+        } else {
+          res.json({"error": "missing required information"});
+        }
+      });
 };
